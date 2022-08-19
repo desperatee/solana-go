29d413b570437bfcd48a228d269d05fc00193e78
DashAIO <srakorbot@gmail.com>
2022-08-19 20:04:47 +0200

    add compute budget

---
+++ programs/compute_budget/RequestUnits.go
@@ -0,0 +1,146 @@
+// Copyright 2021 github.com/gagliardetto
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package compute_budget
+
+import (
+	"encoding/binary"
+	"errors"
+	ag_solanago "github.com/desperatee/solana-go"
+	ag_format "github.com/desperatee/solana-go/text/format"
+	ag_binary "github.com/gagliardetto/binary"
+	ag_treeout "github.com/gagliardetto/treeout"
+)
+
+// RequestUnits
+type RequestUnits struct {
+	// Number of lamports to transfer to the new account
+	Units *uint64
+	// Number of lamports to transfer to the new account
+	AdditionalFee                *uint64
+	ag_solanago.AccountMetaSlice `bin:"-" borsh_skip:"true"`
+}
+
+// NewTransferInstructionBuilder creates a new `Transfer` instruction builder.
+func NewTransferInstructionBuilder() *RequestUnits {
+	nd := &RequestUnits{
+		AccountMetaSlice: make(ag_solanago.AccountMetaSlice, 0),
+	}
+	return nd
+}
+
+// Number of requested compute units
+func (inst *RequestUnits) SetUnits(units uint64) *RequestUnits {
+	inst.Units = &units
+	return inst
+}
+
+// Additional fee in lamports
+func (inst *RequestUnits) SetAdditionalFee(additionalFee uint64) *RequestUnits {
+	inst.AdditionalFee = &additionalFee
+	return inst
+}
+
+func (inst RequestUnits) Build() *Instruction {
+	return &Instruction{BaseVariant: ag_binary.BaseVariant{
+		Impl:   inst,
+		TypeID: ag_binary.TypeIDFromUint32(Instruction_RequestUnits, binary.LittleEndian),
+	}}
+}
+
+// ValidateAndBuild validates the instruction parameters and accounts;
+// if there is a validation error, it returns the error.
+// Otherwise, it builds and returns the instruction.
+func (inst RequestUnits) ValidateAndBuild() (*Instruction, error) {
+	if err := inst.Validate(); err != nil {
+		return nil, err
+	}
+	return inst.Build(), nil
+}
+
+func (inst *RequestUnits) Validate() error {
+	// Check whether all (required) parameters are set:
+	{
+		if inst.Units == nil {
+			return errors.New("Units parameter is not set")
+		}
+		if inst.AdditionalFee == nil {
+			return errors.New("AdditionalFee parameter is not set")
+		}
+	}
+	return nil
+}
+
+func (inst *RequestUnits) EncodeToTree(parent ag_treeout.Branches) {
+	parent.Child(ag_format.Program(ProgramName, ProgramID)).
+		//
+		ParentFunc(func(programBranch ag_treeout.Branches) {
+			programBranch.Child(ag_format.Instruction("RequestUnits")).
+				//
+				ParentFunc(func(instructionBranch ag_treeout.Branches) {
+
+					// Parameters of the instruction:
+					instructionBranch.Child("Params").ParentFunc(func(paramsBranch ag_treeout.Branches) {
+						paramsBranch.Child(ag_format.Param("Units", *inst.Units))
+						paramsBranch.Child(ag_format.Param("AdditionalFee", *inst.AdditionalFee))
+					})
+				})
+		})
+}
+
+func (inst RequestUnits) MarshalWithEncoder(encoder *ag_binary.Encoder) error {
+	// Serialize `RequestedComputeUnits` param:
+	{
+		err := encoder.Encode(*inst.Units)
+		if err != nil {
+			return err
+		}
+	}
+	// Serialize `AdditionalFee` param:
+	{
+		err := encoder.Encode(*inst.AdditionalFee)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (inst *RequestUnits) UnmarshalWithDecoder(decoder *ag_binary.Decoder) error {
+	// Deserialize `RequestedComputeUnits` param:
+	{
+		err := decoder.Decode(&inst.Units)
+		if err != nil {
+			return err
+		}
+	}
+	// Deserialize `AdditionalFee` param:
+	{
+		err := decoder.Decode(&inst.AdditionalFee)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// NewRequestUnitsInstruction declares a new RequestUnits instruction with the provided parameters and accounts.
+func NewRequestUnitsInstruction(
+	// Parameters:
+	requestedComputeUnits uint64,
+	additionalFee uint64) *RequestUnits {
+	return NewTransferInstructionBuilder().
+		SetUnits(requestedComputeUnits).
+		SetAdditionalFee(additionalFee)
+}
---
+++ programs/compute_budget/instructions.go
@@ -0,0 +1,133 @@
+// Copyright 2021 github.com/gagliardetto
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Create new accounts, allocate account data, assign accounts to owning programs,
+// transfer lamports from System Program owned accounts and pay transacation fees.
+
+package compute_budget
+
+import (
+	"bytes"
+	"encoding/binary"
+	"fmt"
+
+	ag_spew "github.com/davecgh/go-spew/spew"
+	ag_solanago "github.com/desperatee/solana-go"
+	ag_text "github.com/desperatee/solana-go/text"
+	ag_binary "github.com/gagliardetto/binary"
+	ag_treeout "github.com/gagliardetto/treeout"
+)
+
+var ProgramID ag_solanago.PublicKey = ag_solanago.MustPublicKeyFromBase58("ComputeBudget111111111111111111111111111111")
+
+func SetProgramID(pubkey ag_solanago.PublicKey) {
+	ProgramID = pubkey
+	ag_solanago.RegisterInstructionDecoder(ProgramID, registryDecodeInstruction)
+}
+
+const ProgramName = "ComputeBudget"
+
+func init() {
+	ag_solanago.RegisterInstructionDecoder(ProgramID, registryDecodeInstruction)
+}
+
+const (
+	// Create a new account
+	Instruction_RequestUnits uint32 = iota
+)
+
+// InstructionIDToName returns the name of the instruction given its ID.
+func InstructionIDToName(id uint32) string {
+	switch id {
+	case Instruction_RequestUnits:
+		return "RequestUnits"
+	default:
+		return ""
+	}
+}
+
+type Instruction struct {
+	ag_binary.BaseVariant
+}
+
+func (inst *Instruction) EncodeToTree(parent ag_treeout.Branches) {
+	if enToTree, ok := inst.Impl.(ag_text.EncodableToTree); ok {
+		enToTree.EncodeToTree(parent)
+	} else {
+		parent.Child(ag_spew.Sdump(inst))
+	}
+}
+
+var InstructionImplDef = ag_binary.NewVariantDefinition(
+	ag_binary.Uint32TypeIDEncoding,
+	[]ag_binary.VariantType{
+		{
+			"RequestUnits", (*RequestUnits)(nil),
+		},
+	},
+)
+
+func (inst *Instruction) ProgramID() ag_solanago.PublicKey {
+	return ProgramID
+}
+
+func (inst *Instruction) Accounts() (out []*ag_solanago.AccountMeta) {
+	return inst.Impl.(ag_solanago.AccountsGettable).GetAccounts()
+}
+
+func (inst *Instruction) Data() ([]byte, error) {
+	buf := new(bytes.Buffer)
+	if err := ag_binary.NewBinEncoder(buf).Encode(inst); err != nil {
+		return nil, fmt.Errorf("unable to encode instruction: %w", err)
+	}
+	return buf.Bytes(), nil
+}
+
+func (inst *Instruction) TextEncode(encoder *ag_text.Encoder, option *ag_text.Option) error {
+	return encoder.Encode(inst.Impl, option)
+}
+
+func (inst *Instruction) UnmarshalWithDecoder(decoder *ag_binary.Decoder) error {
+	return inst.BaseVariant.UnmarshalBinaryVariant(decoder, InstructionImplDef)
+}
+
+func (inst Instruction) MarshalWithEncoder(encoder *ag_binary.Encoder) error {
+	err := encoder.WriteUint32(inst.TypeID.Uint32(), binary.LittleEndian)
+	if err != nil {
+		return fmt.Errorf("unable to write variant type: %w", err)
+	}
+	return encoder.Encode(inst.Impl)
+}
+
+func registryDecodeInstruction(accounts []*ag_solanago.AccountMeta, data []byte) (interface{}, error) {
+	inst, err := DecodeInstruction(accounts, data)
+	if err != nil {
+		return nil, err
+	}
+	return inst, nil
+}
+
+func DecodeInstruction(accounts []*ag_solanago.AccountMeta, data []byte) (*Instruction, error) {
+	inst := new(Instruction)
+	if err := ag_binary.NewBinDecoder(data).Decode(inst); err != nil {
+		return nil, fmt.Errorf("unable to decode instruction: %w", err)
+	}
+	if v, ok := inst.Impl.(ag_solanago.AccountsSettable); ok {
+		err := v.SetAccounts(accounts)
+		if err != nil {
+			return nil, fmt.Errorf("unable to set accounts for instruction: %w", err)
+		}
+	}
+	return inst, nil
+}
