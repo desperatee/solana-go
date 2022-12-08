e683957625e5dca259696127f3c8671fa2870319
DashAIO <srakorbot@gmail.com>
2022-12-08 17:08:04 +0100

    quic cleanup

--- rpc/tpu_quic/tpu_client.go
+++ rpc/tpu_quic/tpu_client.go
@@ -229,25 +229,6 @@ func (leaderTPUService *LeaderTPUService) LeaderTPUSocketsWithConn(fanoutSlots u
 				InsecureSkipVerify: true,
 				NextProtos:         []string{"solana-tpu"},
 				Certificates:       []tls.Certificate{cert},
-				//ServerName:         "Solana node",
-				//CipherSuites: []uint16{
-				//	tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
-				//	tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
-				//	tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256,
-				//	tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256,
-				//	tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
-				//	tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
-				//	tls.TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA,
-				//	tls.TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA,
-				//	tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305,
-				//	tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256,
-				//},
-				//CurvePreferences: []tls.CurveID{
-				//	tls.X25519,
-				//	tls.CurveP256,
-				//	tls.CurveP384,
-				//	tls.CurveP521,
-				//},
 			}, &quic.Config{
 				KeepAlivePeriod:            1 * time.Second,
 				MaxIdleTimeout:             2 * time.Second,
@@ -273,34 +254,11 @@ func (leaderTPUService *LeaderTPUService) LeaderTPUSocketsWithConn(fanoutSlots u
 		if connection != nil {
 			conns = append(conns, connection)
 		}
-		//streamTries := 0
-		//var stream quic.SendStream
-		//for {
-		//	strm, err := connection.OpenUniStream()
-		//	if err != nil {
-		//		fmt.Println(err)
-		//		if streamTries < 3 {
-		//			streamTries++
-		//			continue
-		//		} else {
-		//			break
-		//		}
-		//	}
-		//	stream = strm
-		//	break
-		//}
-		//if stream != nil {
-		//	streams = append(streams, stream)
-		//}
 	}
 	for _, old := range leaderTPUService.LeaderConnections {
 		old.CloseWithError(0, "")
 	}
-	//for _, old := range leaderTPUService.LeaderStreams {
-	//	old.Close()
-	//}
 	leaderTPUService.LeaderConnections = conns
-	//leaderTPUService.LeaderStreams = streams
 	return sockets
 }
 
@@ -326,25 +284,6 @@ func (leaderTPUService *LeaderTPUService) GetLeaderConnections(fanoutSlots uint6
 				InsecureSkipVerify: true,
 				NextProtos:         []string{"solana-tpu"},
 				Certificates:       []tls.Certificate{cert},
-				//ServerName:         "Solana node",
-				//CipherSuites: []uint16{
-				//	tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
-				//	tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
-				//	tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256,
-				//	tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256,
-				//	tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
-				//	tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
-				//	tls.TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA,
-				//	tls.TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA,
-				//	tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305,
-				//	tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256,
-				//},
-				//CurvePreferences: []tls.CurveID{
-				//	tls.X25519,
-				//	tls.CurveP256,
-				//	tls.CurveP384,
-				//	tls.CurveP521,
-				//},
 			}, &quic.Config{
 				KeepAlivePeriod:            1 * time.Second,
 				MaxIdleTimeout:             2 * time.Second,
@@ -369,25 +308,6 @@ func (leaderTPUService *LeaderTPUService) GetLeaderConnections(fanoutSlots uint6
 		if connection != nil {
 			conns = append(conns, connection)
 		}
-		//streamTries := 0
-		//var stream quic.SendStream
-		//for {
-		//	strm, err := connection.OpenUniStream()
-		//	if err != nil {
-		//		fmt.Println(err)
-		//		if streamTries < 3 {
-		//			streamTries++
-		//			continue
-		//		} else {
-		//			break
-		//		}
-		//	}
-		//	stream = strm
-		//	break
-		//}
-		//if stream != nil {
-		//	streams = append(streams, stream)
-		//}
 	}
 	return conns
 }
